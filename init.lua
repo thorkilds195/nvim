5a6e9a4d88f61f93cad9a8535b95d347c12b1113
thorkilds195 <thorkilds195@gmail.com>
2025-06-22 14:13:17 +0200

    Add shortcut for enabling refs of vairables

--- init.lua
+++ init.lua
@@ -124,6 +124,7 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set('n', '<leader>td', builtin.diagnostics, { desc = 'Telescope diagnostics' })
+vim.keymap.set('n', '<leader>fr', builtin.lsp_references, { desc = 'LSP references' })
 vim.keymap.set('n', '<leader>fd', builtin.lsp_definitions, { desc = 'LSP defs' })
 vim.keymap.set('n', '<leader>tt', ':NvimTreeToggle<cr>')
 vim.keymap.set('n', '<leader>tf', ':NvimTreeFocus<cr>')
